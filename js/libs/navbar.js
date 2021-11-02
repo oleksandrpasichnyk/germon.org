4a10a095cc794e67af09f2be500c4dbac7d1f097
OleksandrPasichnyk <oleksandr.pasichnyk.work@gmail.com>
2021-11-02 23:15:24 +0200

    upgraded gallery

---
+++ js/libs/navbar.js
@@ -0,0 +1,49 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+
+function navbar() {
+
+(function () {
+   'use strict';
+
+   /* ==============================================
+  	Values Slider
+  	=============================================== */ 
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 100);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height()/2 - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+}());
+
+
+}
+navbar();
