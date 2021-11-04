7844c95be9456de156230e517ec249e9ef53e510
OleksandrPasichnyk <oleksandr.pasichnyk.work@gmail.com>
2021-11-04 19:23:34 +0200

    upgraded localization, values texts, sync calendar

--- js/calendar.js
+++ js/calendar.js
@@ -15,10 +15,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // THIS KEY WON'T WORK IN PRODUCTION!!!
     // To make your own Google API key, follow the directions here:
     // http://fullcalendar.io/docs/google_calendar/
-    googleCalendarApiKey: 'AIzaSyDszSRTiBfYEssyCXH5vUua-tuyyI81if0',
+    googleCalendarApiKey: 'AIzaSyDSBCaOhrRViBVidxG2Mu7bkCGBRmerMGI',
 
     // US Holidays
-    events: '1hh41n3ao1l6n8lsnircltnij4@group.calendar.google.com',
+    // events: '1hh41n3ao1l6n8lsnircltnij4@group.calendar.google.com',
+    events: {
+      googleCalendarId: 'mount.germon@gmail.com',
+      // className: 'gcal-event' // an option!
+    },
 
     eventClick: function(arg) {
       // opens events in a popup window
